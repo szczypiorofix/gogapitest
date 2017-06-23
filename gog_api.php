c329076971dd5899355c64d4de936e6eabe884fb
szczypiorofix <szczypiorofix@o2.pl>
2017-06-23 16:13:17 +0200

    Dalsze próby uzyskania danych o użytkowniku. Dane na temat gry uzyskiwan sa już poprawnie.

--- gog_api.php
+++ gog_api.php
@@ -10,12 +10,21 @@ class App
     public static function init() {
         $gogApi = new GogApiApp();
         self::$output = $gogApi->getContent('GET', '1440163901'); // INSERT GAME ID HERE
+        self::$output .= $gogApi->getUserData('GET', '842773838996');
     }
     
     public static function showOutput() {
         //echo '<pre>'.self::$output.'</pre>';
         echo self::$output;
     }
+}
+
+class GogUser
+{
+    public $userId;
+    public $username;
+    
+    public function __construct() {}
 }
 
 class GogGameContent
@@ -31,7 +40,6 @@ class GogGameContent
 
 class GogApiApp
 {
-
     public function __construct() {}
     
     private function getDataFromAPI($url, $type, $args) {
@@ -63,6 +71,43 @@ class GogApiApp
         
         // JS - how to get Trello Token from Local Storage
         //console.log(localStorage.trello_token);
+    }
+    
+    public function getUserData($type, $request, $args = false)
+    {
+        if (!$args) {
+            $args = array();
+        } elseif (!is_array($args)) {
+            $args = array($args);
+        }
+
+         // http://api.gog.com/products/1207658691?expand=description
+        $url = 'users.gog.com/users/' . $request;
+        $dataFromAPI = $this->getDataFromAPI($url, $type, $args);
+        $gogUser = new GogUser();
+        $content = '';
+        
+//        if ($dataFromAPI !== '') {
+//            $gogGameContent->id = $dataFromAPI->id;
+//            $gogGameContent->title = $dataFromAPI->title;
+//            $gogGameContent->description = $dataFromAPI->description;
+//            $gogGameContent->languages = get_object_vars($dataFromAPI->languages);
+//            
+//            $content .= '<p class="stat"><b>Name</b></p><h3>'.$gogGameContent->title.'</h3>';
+//            $content .= '<p class="stat"><b>ID</b></p><p>'.$gogGameContent->id.'</p>';
+//            $content .= '<p class="stat"><b>Languages</b></p><p>';
+//            foreach ($gogGameContent->languages as $lang) {
+//                $content .= $lang.', ';
+//            }
+//            $content .= '</p>';
+//            $content .= '<p class="stat"><b>Description (short)</b></p><p>'.$gogGameContent->description->lead.'</p>';
+//            $content .= '<p class="stat"><b>Description (full)</b></p><p>'.$gogGameContent->description->full.'</p>';
+//            $content .= '<p class="stat"><b>What is cool about this game?</b></p><p>'.$gogGameContent->description->whats_cool_about_it.'</p>';
+//        }
+        
+        $content = print_r($dataFromAPI);
+        
+        return $content;
     }
     
     public function getContent($type, $request, $args = false)
